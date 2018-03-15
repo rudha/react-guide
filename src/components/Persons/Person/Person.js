f983fb43e654de83a7a8a186b62bffb6687c3e25
Rudha Oliveira <rudha94@gmail.com>
2018-03-15 19:31:29 -0300

    Implemented component lifecycle.

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css'
 
-const person = (props) => {
-	return (
-		<div className={classes.Person} >
-			<p onClick={props.click}>I'm {props.name}, and I'm {props.age}!</p>
-			<p>{props.children}</p>
-			<input type="text" onChange={props.changed} value={props.name} />
-		</div>
-	)
-};
+class Person extends Component {
+	constructor(props) {
+		super(props);
+		console.log('[Person.js] Inside constructor', props);
+	};
+	
+	componentWillMount() {
+		console.log('[Person.js] Inside componentWillMount()');
+	}
+	
+	componentDidMount() {
+		console.log('[Person.js] Inside componentDidMount()');
+	}
 
-export default person;
+	componentWillUnmount() {
+		// Component is about to get removed => Perform any cleanup work here!
+		console.log('[componentWillUnmount()] I\'m about to be removed!');
+	}
+	
+	render () {
+		console.log('[Person.js] Inside render()');
+		return (
+			<div className={classes.Person} >
+				<p onClick={this.props.click}>I'm {this.props.name}, and I'm {this.props.age}!</p>
+				<p>{this.props.children}</p>
+				<input type="text" onChange={this.props.changed} value={this.props.name} />
+			</div>
+		)
+	}
+}
+
+export default Person;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,52 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit'
 
-class App extends Component {
-  state = {
-    persons: [
-      { id: 'fasd', name: 'Max', age: 18 },
-      { id: 'adsf', name: 'Chloe', age: 19},
-      { id: 'afds', name: 'Steph', age: 18}
-    ],
-    showPersons: false
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    console.log('[App.js] Inside constructor');
+    this.state = { // Initializing state in constructor...
+      persons: [
+        { id: 'fasd', name: 'Max', age: 18 },
+        { id: 'adsf', name: 'Chloe', age: 19},
+        { id: 'afds', name: 'Steph', age: 18}
+      ],
+      showPersons: false
+    };
   }
+
+  componentWillMount() {
+    console.log('[App.js] Inside componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('[App.js] Inside componentDidMount()');
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+	// 	console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+	// 	return nextState.persons !== this.state.persons ||
+  //   nextState.showPersons !== this.state.showPersons;
+	// }
+
+	componentWillUpdate(nextProps, nextState) {
+		console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
+	}
+
+	componentDidUpdate () {
+		console.log('[UPDATE App.js] Inside componentDidUpdate');
+	}
+  
+  // state = { // As opposed to initializing it here
+  //   persons: [
+  //     { id: 'fasd', name: 'Max', age: 18 },
+  //     { id: 'adsf', name: 'Chloe', age: 19},
+  //     { id: 'afds', name: 'Steph', age: 18}
+  //   ],
+  //   showPersons: false
+  // }
 
   deletePersonHandler = (index) => {
     //const persons = this.state.persons.slice(); // Copy the original array
@@ -43,6 +78,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App.js] Inside render()');
     let persons = null;
 
     if (this.state.showPersons) {
@@ -55,6 +91,7 @@ class App extends Component {
 
     return (
       <div className={classes.App}>
+        <button onClick={() => {this.setState({showPersons: true})}} >Show Persons</button>
         <Cockpit
         showPersons={this.showPersons}
         persons={this.state.persons}

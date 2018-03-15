6e830b43319dd9758f592185019eb168d896eb82
Rudha Oliveira <rudha94@gmail.com>
2018-03-15 16:36:16 -0300

    Ejected and inplemented CSS modules.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -42,16 +42,8 @@ class App extends Component {
   }
 
   render() {
-    const styleButton = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid black',
-      padding: '8px',
-      cursor: 'pointer',
-    };
-
     let persons = null;
+    let buttonClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -66,24 +58,23 @@ class App extends Component {
           })}
         </div>
       );
-
-      styleButton.backgroundColor = 'red';
+      buttonClass = classes.Red;
     }
 
-    const classes = [];
+    const buttonClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      buttonClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      buttonClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>I'm a React App!</h1>
-        <p className={classes.join(' ')} >This works</p>
+        <p className={buttonClasses.join(' ')} >This works</p>
         <button
-          style={styleButton}
+          className={buttonClass}
           onClick={this.togglePersonsHandler}>Toggle Persons
         </button>
         {persons}
